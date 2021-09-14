115afa3b025ffe934a4fd508f04d3a1b3b78a566
marcopeg <marco.pegoraro@gmail.com>
2021-09-14 06:04:31 +0000

    Apply a custom schedule to a document while creating it
    #53

--- ssr/feature/api-v1/routes/v1-document-create.test.e2e.js
+++ ssr/feature/api-v1/routes/v1-document-create.test.e2e.js
@@ -21,6 +21,26 @@ describe('v1DocumentCreate', () => {
       expect(r1.data.doc.subject).toBe('d1');
       expect(r1.data.doc.payload.a).toBe(1);
     });
+
+    it('should create a new document with a custom schedule', async () => {
+      await global.fetchq.query(`SELECT * FROM fetchq.queue_create('q1')`);
+
+      const docDate = '2027-01-10 12:35:44';
+
+      const r1 = await global.post(`/api/v1/queues/q1/docs`, {
+        subject: 'd1',
+        payload: { a: 1 },
+        next_iteration: docDate,
+      });
+
+      expect(r1.data.op).toBe('push');
+      expect(r1.data.doc.subject).toBe('d1');
+      expect(r1.data.doc.payload.a).toBe(1);
+
+      const d2 = new Date(docDate).toString();
+      const d1 = new Date(r1.data.doc.next_iteration).toString();
+      expect(d1 === d2).toBe(true);
+    });
   });
 
   describe('append', () => {
